aae17fb592a47758fc63821337fa3697aa8dc6c0
Tammy Lam <lamtammy1515@gmail.com>
2019-12-11 20:34:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,18 @@
 
 # Write your code here.
 
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(array)
+  speaking = []
+  array.each do |name|
+    speaking.push("Hello, my name is #{name}.")
+  end
+  return speaking
+end
