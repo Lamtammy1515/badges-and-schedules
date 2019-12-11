47d92aa90cd0f24e69c0bb060b197f5302787102
Tammy Lam <lamtammy1515@gmail.com>
2019-12-11 20:36:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,3 +26,13 @@ def assign_rooms(array)
   end
   return speaker
 end
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end 
